321509c9565e76f541041cd8d433a046943503f5
raystef66 <s.vanbarel@gmail.com>
2019-09-17 08:21:14 +0000

    bringup RR

--- rr_axon7.mk
+++ rr_axon7.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017-2018 The LineageOS Project
@@ -23,13 +24,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/zte/axon7/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_axon7
+PRODUCT_NAME := rr_axon7
 PRODUCT_DEVICE := axon7
 PRODUCT_BRAND := ZTE
-PRODUCT_MODEL := ZTE A2017U
+PRODUCT_MODEL := ZTE A2017X
 PRODUCT_MANUFACTURER := ZTE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -38,6 +39,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="P996A01_O-user 8.0.0 OPR1.170623.032 28 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "ZTE/P996A01_O/ailsa_ii:8.0.0/OPR1.170623.032/20180815.171922:user/release-keys"
+BUILD_FINGERPRINT := "google/crosshatch/crosshatch:9/PQ2A.190405.003/5310204:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-zte
+
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo rr_axon7-userdebug
+
